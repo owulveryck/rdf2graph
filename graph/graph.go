5e2274ed16292e5961e989e025249c154869c7f5
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2020-11-06 16:07:02 +0100

    feat: code refacto

---
+++ graph/graph.go
@@ -0,0 +1,127 @@
+package graph
+
+import (
+	"log"
+
+	rdf "github.com/deiu/gon3"
+	"gonum.org/v1/gonum/graph"
+	"gonum.org/v1/gonum/graph/simple"
+)
+
+type dict map[string]rdf.Term
+
+func (d dict) getOrInsert(t rdf.Term) rdf.Term {
+	if t, ok := d[t.RawValue()]; ok {
+		return t
+	}
+	d[t.RawValue()] = t
+	return t
+}
+
+// NewGraph ...
+func NewGraph(rdfGraph *rdf.Graph) Graph {
+
+	// tree is a map of subjects, containing predicates referencing objects
+	tree := make(map[rdf.Term]map[rdf.Term][]rdf.Term)
+	termDict := dict(make(map[string]rdf.Term))
+	for s := range rdfGraph.IterTriples() {
+		subject := termDict.getOrInsert(s.Subject)
+		predicate := termDict.getOrInsert(s.Predicate)
+		object := termDict.getOrInsert(s.Object)
+		if _, ok := tree[subject]; !ok {
+			tree[subject] = make(map[rdf.Term][]rdf.Term)
+		}
+		sub := tree[subject]
+		if sub[predicate] == nil {
+			sub[predicate] = make([]rdf.Term, 0)
+		}
+		sub[predicate] = append(sub[predicate], object)
+	}
+	g := simple.NewDirectedGraph()
+	reference := make(map[rdf.Term]*Node, len(tree))
+	// create the nodes
+	for s, po := range tree {
+		n := &Node{
+			id:             g.NewNode().ID(),
+			Subject:        s,
+			PredicatObject: po,
+		}
+		g.AddNode(n)
+		reference[s] = n
+	}
+	// create the edges
+	for s, po := range tree {
+
+		me, ok := reference[s]
+		if !ok {
+			log.Fatal("wot? node is not found")
+		}
+		for predicate, objects := range po {
+			for _, object := range objects {
+				if to, ok := reference[object]; ok {
+					if me == to {
+						continue
+					}
+					e := edge{
+						F:    me,
+						T:    to,
+						term: predicate,
+					}
+					g.SetEdge(e)
+				}
+			}
+
+		}
+	}
+	return Graph{
+		DirectedGraph: g,
+		Reference:     reference,
+	}
+}
+
+// Graph is carrying the information
+type Graph struct {
+	*simple.DirectedGraph
+	// Reference of the term and their associated nodes
+	Reference map[rdf.Term]*Node
+}
+
+// Node is a node of the graph, it carries n tuple associated with one subject
+type Node struct {
+	id             int64
+	Subject        rdf.Term
+	PredicatObject map[rdf.Term][]rdf.Term
+}
+
+// ID of the node
+func (n *Node) ID() int64 {
+	return n.id
+}
+
+type edge struct {
+	F, T graph.Node
+	term rdf.Term
+}
+
+func (e edge) From() graph.Node {
+	return e.F
+}
+
+func (e edge) To() graph.Node {
+	return e.T
+}
+
+// ReversedEdge returns a new Edge with the F and T fields
+// swapped.
+func (e edge) ReversedEdge() graph.Edge { return edge{F: e.T, T: e.F, term: e.term} }
+
+// FindNode returns a node whose Term match t's rawstring
+// it returns nil if no matching node is found
+func (g *Graph) FindNode(t rdf.Term) *Node {
+	for term, n := range g.Reference {
+		if term.Equals(t) {
+			return n
+		}
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -6,8 +6,7 @@ import (
 	"os"
 
 	rdf "github.com/deiu/gon3"
-	"gonum.org/v1/gonum/graph"
-	"gonum.org/v1/gonum/graph/simple"
+	"github.com/owulveryck/rdf2raph/graph"
 )
 
 type dict map[string]rdf.Term
@@ -30,9 +29,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	g := NewGraph(parser)
+	g := graph.NewGraph(parser)
 
-	n, ok := findID(g.Reference, rdf.NewIRI("http://schema.org/PostalAddress"))
+	n, ok := g.FindNode(rdf.NewIRI("http://schema.org/PostalAddress"))
 	if !ok {
 		log.Fatal("not found")
 	}
@@ -44,104 +43,3 @@ func main() {
 		fmt.Printf("%v -> %v -> %v\n", n.Subject, e.term, to.Subject)
 	}
 }
-
-func NewGraph(rdfGraph *rdf.Graph) Graph {
-
-	// tree is a map of subjects, containing predicates referencing objects
-	tree := make(map[rdf.Term]map[rdf.Term][]rdf.Term)
-	termDict := dict(make(map[string]rdf.Term))
-	for s := range rdfGraph.IterTriples() {
-		subject := termDict.getOrInsert(s.Subject)
-		predicate := termDict.getOrInsert(s.Predicate)
-		object := termDict.getOrInsert(s.Object)
-		if _, ok := tree[subject]; !ok {
-			tree[subject] = make(map[rdf.Term][]rdf.Term)
-		}
-		sub := tree[subject]
-		if sub[predicate] == nil {
-			sub[predicate] = make([]rdf.Term, 0)
-		}
-		sub[predicate] = append(sub[predicate], object)
-	}
-	g := simple.NewDirectedGraph()
-	reference := make(map[rdf.Term]*node, len(tree))
-	// create the nodes
-	for s, po := range tree {
-		n := &node{
-			id:             g.NewNode().ID(),
-			Subject:        s,
-			PredicatObject: po,
-		}
-		g.AddNode(n)
-		reference[s] = n
-	}
-	// create the edges
-	for s, po := range tree {
-
-		me, ok := reference[s]
-		if !ok {
-			log.Fatal("wot? node is not found")
-		}
-		for predicate, objects := range po {
-			for _, object := range objects {
-				if to, ok := reference[object]; ok {
-					if me == to {
-						continue
-					}
-					e := edge{
-						F:    me,
-						T:    to,
-						term: predicate,
-					}
-					g.SetEdge(e)
-				}
-			}
-
-		}
-	}
-	return Graph{
-		DirectedGraph: g,
-		Reference:     reference,
-	}
-}
-
-type Graph struct {
-	*simple.DirectedGraph
-	Reference map[rdf.Term]*node
-}
-
-func findID(dict map[rdf.Term]*node, t rdf.Term) (*node, bool) {
-	for term, n := range dict {
-		if term.Equals(t) {
-			return n, true
-		}
-	}
-	return nil, false
-}
-
-type node struct {
-	id             int64
-	Subject        rdf.Term
-	PredicatObject map[rdf.Term][]rdf.Term
-}
-
-func (n *node) ID() int64 {
-	return n.id
-}
-
-type edge struct {
-	F, T graph.Node
-	term rdf.Term
-}
-
-func (e edge) From() graph.Node {
-	return e.F
-}
-
-func (e edge) To() graph.Node {
-	return e.T
-}
-
-// ReversedEdge returns a new Edge with the F and T fields
-// swapped.
-func (e edge) ReversedEdge() graph.Edge { return edge{F: e.T, T: e.F, term: e.term} }
