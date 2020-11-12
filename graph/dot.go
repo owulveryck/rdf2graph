784ca6acb4dbc068e6ae489ba4091f46438de41c
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2020-11-12 09:56:55 +0100

    feat: attributes for graph

--- graph/dot.go
+++ graph/dot.go
@@ -1,16 +1,41 @@
 package graph
 
-import "gonum.org/v1/gonum/graph/encoding"
+import (
+	"bytes"
+	"html"
+	"log"
+	"strings"
+	"text/template"
+
+	"gonum.org/v1/gonum/graph/encoding"
+)
 
 // Attributes fulfills the gonum encoding Attributer
 func (n *Node) Attributes() []encoding.Attribute {
+	replaceSlash := func(s string) string {
+		return strings.Replace(s, `/`, `\/`, -1)
+	}
+	funcMap := template.FuncMap{
+		// The name "title" is what the function will be called in the template text.
+		"wrap":       wrap,
+		"htmlescape": html.EscapeString,
+		"slash":      replaceSlash,
+	}
+	t := template.Must(template.New("node").Funcs(funcMap).Parse(tmpl))
+	var b bytes.Buffer
+	err := t.Execute(&b, n)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	shape := encoding.Attribute{
 		Key:   "shape",
-		Value: "record",
+		Value: "plain",
 	}
 	label := encoding.Attribute{
-		Key:   "label",
-		Value: n.Subject.RawValue(),
+		Key: "label",
+		//Value: n.Subject.RawValue(),
+		Value: b.String(),
 	}
 	/*	type temp struct {
 			Subject string
@@ -22,6 +47,10 @@ func (n *Node) Attributes() []encoding.Attribute {
 
 // Attributes fulfills the gonum encoding Attributer
 func (e Edge) Attributes() []encoding.Attribute {
+	shape := encoding.Attribute{
+		Key:   "shape",
+		Value: "plain",
+	}
 	label := encoding.Attribute{
 		Key:   "label",
 		Value: e.Term.String(),
@@ -30,14 +59,35 @@ func (e Edge) Attributes() []encoding.Attribute {
 		Key:   "fontsize",
 		Value: "10",
 	}
-	return []encoding.Attribute{label, fontsize}
+	return []encoding.Attribute{label, fontsize, shape}
 }
 
-const tmpl = `<
-	 <table border='1' cellborder='0'>
-	   {{range $key, $value := .Val}}
-	   <tr><td>{{$key}}</td><td>{{$value}}</td></tr>
-	   {{end}}
-     </table>
+func wrap(text string) string {
+	lineWidth := 35
+	words := strings.Fields(strings.TrimSpace(text))
+	if len(words) == 0 {
+		return text
+	}
+	wrapped := words[0]
+	spaceLeft := lineWidth - len(wrapped)
+	for _, word := range words[1:] {
+		if len(word)+1 > spaceLeft {
+			wrapped += `"<br/>"` + word
+			spaceLeft = lineWidth - len(word)
+		} else {
+			wrapped += " " + word
+			spaceLeft -= 1 + len(word)
+		}
+	}
+	return wrapped
+}
 
->`
+const tmpl = `<<table border='1' cellborder='1'>
+	   <tr><td colspan="2" bgcolor="lightblue">"{{ .Subject.RawValue | htmlescape}}"</td></tr>
+	   <tr><td bgcolor="lightgrey">Predicate</td><td bgcolor="lightgrey">object</td></tr>
+	   {{range $key, $value := .PredicateObject -}}
+	   {{range $value -}}
+	   <tr><td>"{{ $key.RawValue | htmlescape }}"</td><td>"{{.RawValue | htmlescape | wrap }}"</td></tr>
+	   {{end -}}
+	   {{end -}}
+	 </table>>`
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	rdf "github.com/owulveryck/gon3"
 	"github.com/owulveryck/rdf2graph/graph"
+	"gonum.org/v1/gonum/graph/encoding/dot"
 )
 
 type dict map[string]rdf.Term
@@ -29,56 +30,58 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(parser.GetNamespaces())
 	g := graph.NewGraph(gr)
-	rdfType, ok := g.Dict["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"]
-	if !ok {
-		log.Fatal("Term not present in the graph: ", "rdfType")
-	}
-	schemaOrgRangeInclude, ok := g.Dict["http://schema.org/rangeIncludes"]
-	if !ok {
-		log.Fatal("Term not present in the graph: ", "rangeInclude")
-	}
-	schemaOrgDomainIncludes, ok := g.Dict["http://schema.org/domainIncludes"]
-	if !ok {
-		log.Fatal("Term not present in the graph: ", "domainIncludes")
-	}
-	rdfsSubClassOf, ok := g.Dict["http://www.w3.org/2000/01/rdf-schema#subClassOf"]
-	if !ok {
-		log.Fatal("Term not present in the graph: ", "rdfsSubClassOf")
-	}
-	rdfProperty, ok := g.Dict["http://www.w3.org/1999/02/22-rdf-syntax-ns#Property"]
-	if !ok {
-		log.Fatal("Term not present in the graph: ", "rdfProperty")
-	}
-
-	n := g.FindNode(rdf.NewIRI(os.Args[1]))
-	if n == nil {
-		log.Fatal("not found")
-	}
-	fmt.Println(n.Subject)
-	for k, v := range n.PredicateObject {
-		n := g.FindNode(k)
-		if n == nil {
-			fmt.Printf("\t%v: %v\n", k, v)
-		}
-	}
-	it := g.DirectedGraph.To(n.ID())
-	for it.Next() {
-		to := it.Node().(*graph.Node)
-		e := g.Edge(to.ID(), n.ID()).(graph.Edge)
-		fmt.Printf("\t\t-> %v -> %v\n", e.Term, to.Subject)
-	}
 	/*
-		b, err := dot.Marshal(g.DirectedGraph, "test", " ", " ")
-		if err != nil {
-			log.Fatal(err)
+		fmt.Println(parser.GetNamespaces())
+		rdfType, ok := g.Dict["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"]
+		if !ok {
+			log.Fatal("Term not present in the graph: ", "rdfType")
 		}
-		fmt.Println(string(b))
+		schemaOrgRangeInclude, ok := g.Dict["http://schema.org/rangeIncludes"]
+		if !ok {
+			log.Fatal("Term not present in the graph: ", "rangeInclude")
+		}
+		schemaOrgDomainIncludes, ok := g.Dict["http://schema.org/domainIncludes"]
+		if !ok {
+			log.Fatal("Term not present in the graph: ", "domainIncludes")
+		}
+		rdfsSubClassOf, ok := g.Dict["http://www.w3.org/2000/01/rdf-schema#subClassOf"]
+		if !ok {
+			log.Fatal("Term not present in the graph: ", "rdfsSubClassOf")
+		}
+		rdfProperty, ok := g.Dict["http://www.w3.org/1999/02/22-rdf-syntax-ns#Property"]
+		if !ok {
+			log.Fatal("Term not present in the graph: ", "rdfProperty")
+		}
+
+		n := g.FindNode(rdf.NewIRI(os.Args[1]))
+		if n == nil {
+			log.Fatal("not found")
+		}
+		fmt.Println(n.Subject)
+		for k, v := range n.PredicateObject {
+			n := g.FindNode(k)
+			if n == nil {
+				fmt.Printf("\t%v: %v\n", k, v)
+			}
+		}
+		it := g.DirectedGraph.To(n.ID())
+		for it.Next() {
+			to := it.Node().(*graph.Node)
+			e := g.Edge(to.ID(), n.ID()).(graph.Edge)
+			fmt.Printf("\t\t-> %v -> %v\n", e.Term, to.Subject)
+		}
 	*/
-	_ = rdfProperty
-	_ = rdfType
-	_ = schemaOrgDomainIncludes
-	_ = schemaOrgRangeInclude
-	_ = rdfsSubClassOf
+	b, err := dot.Marshal(g.DirectedGraph, "test", " ", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(b))
+	/*
+		_ = rdfProperty
+		_ = rdfType
+		_ = schemaOrgDomainIncludes
+		_ = schemaOrgRangeInclude
+		_ = rdfsSubClassOf
+	*/
 }
